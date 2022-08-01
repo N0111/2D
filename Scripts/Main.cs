1c665e639577e17577994380695cc387f2d4258c
N0111 <95976517+N0111@users.noreply.github.com>
2022-08-01 19:28:30 +0300

    Add files via upload

---
+++ Scripts/Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class Main : MonoBehaviour
+    {
+        private SpriteAnimatorController _playerAnimator;
+        private AnimationConfig _config;
+        [SerializeField] private LevelObjectView _playerView;
+
+        private void Awake()
+        {
+            _config = Resources.Load<AnimationConfig>("SpriteAnimCfg");
+            _playerAnimator = new SpriteAnimatorController(_config);
+            _playerAnimator.StartAnimation(_playerView._renderer, AnimState.Run, true, 10);
+        }
+
+        void Update()
+        {
+            _playerAnimator.Update();
+        }
+    }
+}
