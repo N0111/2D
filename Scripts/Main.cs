a9c410615afa1079b2c20509657390300a5e0042
N0111 <95976517+N0111@users.noreply.github.com>
2022-08-04 16:45:38 +0300

    Add files via upload

---
+++ Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class Main : MonoBehaviour
+    {
+
+        [SerializeField] private LevelObjectView _playerView;
+        private PlayerController _playerController;
+
+        private void Awake()
+        {
+            _playerController = new PlayerController(_playerView);
+        }
+
+        void Update()
+        {
+            _playerController.Update();
+        }
+    }
+}
